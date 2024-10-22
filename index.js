89d08cd631a5fb76967fe24c358412abcacbcb9d
wizardio29928 <142822906+wizardio29928@users.noreply.github.com>
2024-10-22 14:28:17 +0300

    homework

---
+++ index.js
@@ -0,0 +1,28 @@
+const age = 15
+console.log(age);
+
+const name = "Sviatoslav"
+console.log(name);
+
+const isStudent = true
+console.log(isStudent);
+
+const  myString = "Успіх — це вміння рухатись від невдачі до невдачі, не втрачаючи ентузіазму. Вінстон Черчилль"
+console.log(myString);
+
+const b = 10
+const myNumber = a = b + 10;
+console.log(myNumber);
+
+const myNull = null
+console.log(myNull);
+
+const messege = prompt("Як тебе звати")
+console.log(messege);
+
+const data = confirm("Дозволити повідомлення?")
+if (data) {
+    alert ("Дякую за підтвердження!");
+}else{
+    alert ("Дію відмінено!");
+}
